f92fe92c2e220eaabc6656b307ecc475670a3e3d
Sophia Ciocca <sophiaciocca@gmail.com>
2017-05-03 15:47:42 -0400

    finished ch1 woooooo

--- src/pledge.js
+++ src/pledge.js
@@ -5,6 +5,35 @@ Promises Workshop: build the pledge.js ES6-style promise library
 // YOUR CODE HERE:
 
 
+function $Promise (executor) {
+
+    this._state = 'pending';
+    this._value;
+
+    if (typeof executor === 'function') {
+        executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+    }
+    
+}
+
+$Promise.prototype._internalResolve = function(value) {
+// console.log("value", this._value);
+    if (this._state === 'pending') {
+        this._value = value;
+        this._state = 'fulfilled';
+    }
+
+}
+
+$Promise.prototype._internalReject = function(reason) {
+
+    if (this._state === 'pending') {
+        this._value = reason;
+        this._state = 'rejected';
+    }
+    
+}
+
 
 
 
