f5c14974cf688ac929732bc3248d37a20bc4c560
Sophia Ciocca <sophiaciocca@gmail.com>
2017-05-03 18:15:15 -0400

    filled in gaps in Chs 1-3 while engaged in Joe's lecture

--- src/pledge.js
+++ src/pledge.js
@@ -4,6 +4,28 @@ Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+//executor: the callback to the "new Promise" 
+//ex:
+/*
+
+const fs = reqiure ('fs');
+
+new $Promise(function(resolve, reject) {
+    fs.readFile('./1.txt', function (err, contents) {
+        if (err) return reject(err);
+        resolve(contents);
+    })
+});
+
+
+//NOTE:
+executor might not be defined -- like, someone might call new Promise() without an executor.
+
+*/
+const fs = require('fs');
+
+fs.readFile('')
+
 
 function $Promise (executor) {
 
@@ -21,6 +43,8 @@ $Promise.prototype._internalResolve = function(value) {
     if (this._state === 'pending') {
         this._value = value;
         this._state = 'fulfilled';
+        //call callHandlers, to go ahead and get started on the success stuff
+        this._callHandlers();
     }
 }
 
@@ -28,16 +52,12 @@ $Promise.prototype._internalReject = function(reason) {
     if (this._state === 'pending') {
         this._value = reason;
         this._state = 'rejected';
+        //call callHandlers, to go ahead and get started on the error stuff
+        this._callHandlers();
     } 
 }
 
-$Promise.prototype._callHandlers = function() {
-
-    
-
-}
-
-$Promise.prototype.then = function(onFulfilled, onRejected) {
+$Promise.prototype.then = function(successCb, errorCb) {
 
     //building a list
     var newHandler = {
@@ -46,16 +66,51 @@ $Promise.prototype.then = function(onFulfilled, onRejected) {
     };
 
     //if onFulfilled is a function, put it in the array; otherwise, store it as null
-    if (typeof onFulfilled === 'function') {
-        newHandler.successCb = onFulfilled;
+    if (typeof successCb === 'function') {
+        newHandler.successCb = successCb;
     }
 
     //same for onRejected
-    if (typeof onRejected === 'function') {
-        newHandler.errorCb = onRejected;
+    if (typeof errorCb === 'function') {
+        newHandler.errorCb = errorCb;
     }
 
     this._handlerGroups.push(newHandler);
+
+    //if promise is already fulfilled/rejected, call it now! (call callHandlers)
+    if (this._state !== 'pending') {
+        this._callHandlers();
+    }
+
+}
+
+
+$Promise.prototype._callHandlers = function() {
+
+    //if fulfilled, call success handler
+    if (this._state === 'fulfilled') {
+
+        //go thru all of handler groups
+        this._handlerGroups.forEach(function (item) {
+            const successFn = item.successCb;
+            //invoke it 
+            successFn(this._value);
+        }, this) //<<note: use 'this' argument on forEach to keep this intact
+    }
+
+    //otherwise, if rejected, call rejected handler (_callHandlers will never be called if still pending)
+    else {
+        //go thru all of handler groups
+        this._handlerGroups.forEach(function (item) {
+            const failFn = item.errorCb;
+            //invoke it 
+            if (isFn(failFn)) { ///have to check if its a function b/c bubbling -- might sometimes be null
+                failFn(this._value);
+        }, this) //<<note: use 'this' argument on forEach to keep this intact
+    }
+
+    //now, forget about them, after you've dealt with all the goroups
+    this._handlerGroups = [];
 
 }
 
